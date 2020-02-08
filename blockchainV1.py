722480108a2070057e66a20cfca9722ee1037612
VedantDeoghare <59177090+VedantDeoghare@users.noreply.github.com>
2020-02-08 20:36:06 +0530

    Add files via upload

---
+++ blockchainV1.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 09:52:43 2020
+
+@author: KIIT
+"""
+#importing libs
+import datetime
+import hashlib
+import json
+from flask import Flask , jsonify
+#part1 Creating a Block
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = '0')
+        
+    def create_block(self, proof, prev_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': prev_hash}
+        self.chain.append(block)
+        return block
+
+    def get_prev_block(self):
+        return self.chain[-1]
+    
+    def POW(self, prev_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_op = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_op[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof +=1
+        return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        prev_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(prev_block):
+                return False
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_op = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_op[:4] != '0000':
+                return False
+            prev_block = block
+            block_index +=1
+        return True
+    
+# Creating a Web App
+app = Flask(__name__)
+
+#Creating A Blockchain
+blockchain = Blockchain()
+
+#Mining a new Block 
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+    prev_proof = prev_block['proof']
+    proof = blockchain.POW(prev_proof) 
+    prev_hash = blockchain.hash(prev_block)
+    block = blockchain.create_block(proof, prev_hash)
+    response = {'message':'Congragulations you just mined a block',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+    
+ # Getting a full blockchain   
+@app.route('/get_chain', methods=['GET'])
+     
+def get_chain():
+     response = {'chain': blockchain.chain,
+                 'length': len(blockchain.chain)}
+     return jsonify(response), 200
+
+#running the app
+app.run(host = '0.0.0.0', port = 5000)
+    
+    
+    
+    
